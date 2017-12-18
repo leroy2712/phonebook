eb500f1cc1f098fc5c3a3f095e74839973165d7e
Leroy <leroyomondi@gmail.com>
2017-12-18 12:22:27 +0300

    completed project

--- Contact.java
+++ Contact.java
@@ -15,6 +15,29 @@ public class Contact {
     }
 
     public static void main (String[]args){
-        
+        int n = scanner.nextInt();//number of contacts to be added
+        Sring name;//name of contact
+        int number;//phone number of contact
+
+        //loop to insert n number of contacts into details HashMap
+        for (int i = 0; i < n; i++) {
+            name = scanner.next();
+            number = scanner.nextInt();
+            mapDetails(name, number);
+        }
+
+        //loop to check if name entered is in the details hashmap
+        while (scanner.hasNext()) {
+            String search = scanner.next();
+
+            if (details.containsKey(search)) {
+                //if name is found
+                System.out.println(search + "=" + details.get(search));
+            } else {
+                //if name isn't found
+                System.out.println("Not Found");
+            }
+        }
+        scanner.close();
     }
 }
