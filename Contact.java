af5b72f1c516b05843d54226003eba39f9ca5c0b
Leroy <leroyomondi@gmail.com>
2017-12-18 12:19:38 +0300

    defines input method, hashmap and method to store name and phone in key value pairs

--- Contact.java
+++ Contact.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
 public class Contact {
+    //Scanner to get user input
+    static Scanner scanner=new Scanner(System.in);
+
+    //HashMap to store name and number
+    static Map<String,Number>details=new HashMap<String,Integer>();
+
+    //Method that inserts name and number to the details HashMap
+    public static void mapDetails (String entry_name,int entry_number){
+        details.put(entry_name,entry_number);
+    }
+
     public static void main (String[]args){
         
     }
